12bbbd50b616709dbb017074000568b51cdcf7d9
yury-godinho <yury.godinho@technoplus.com.br>
2023-03-30 12:59:27 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace ProjetoSoloCalc
+{
+    class Program
+    {
+        static void Main()
+        {
+            decimal PrimeiroNum = 0;
+            decimal SegundoNum = 0;
+            string Operador;
+            string Final;
+            decimal Valor;
+            
+            Final = "A";
+            while(Final.ToUpper() != "#sair")
+
+                {
+                        Console.WriteLine();
+                        Console.WriteLine("Informe um número: ");
+                        
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal PrimeiroNumwk))
+                        {
+                            PrimeiroNum = PrimeiroNumwk;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor deve ser decimal");
+                        }
+
+                        Console.WriteLine();
+                        Console.WriteLine("Informe a operação: ");
+                        Operador = Console.ReadLine()!;
+                        if (Operador != "*" && Operador != "-" && Operador != "+" && Operador != "/")
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Digite uma operação válida (* , + , - , /)"); 
+                            break;
+                        }
+                        
+                        Console.WriteLine();
+                        Console.WriteLine("Informe outro número: ");
+
+                        if (decimal.TryParse(Console.ReadLine(), out decimal SegundoNumwk))
+                        {
+                            SegundoNum = SegundoNumwk;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("O valor deve ser decimal");
+                        }
+
+                        switch(Operador)
+
+                        {
+                                case "*":
+                                    Valor = PrimeiroNum * SegundoNum;
+                                    Console.WriteLine();
+                                    Console.WriteLine($"Resultado da Multiplicação: {PrimeiroNum} * {SegundoNum} = {Valor}");
+                                    Console.WriteLine();
+                                    Console.WriteLine("Para finalizar digite #SAIR ");
+                                
+                                break;
+
+                                case "+":
+                                    Valor = PrimeiroNum + SegundoNum;
+                                    Console.WriteLine();
+                                    Console.WriteLine($"Resultado da Soma: {PrimeiroNum} + {SegundoNum} = {Valor}");
+                                    Console.WriteLine();
+                                    Console.WriteLine("Para finalizar digite #SAIR ");
+                                
+                                break;
+
+                                case "-":
+                                    Valor = PrimeiroNum - SegundoNum;
+                                    Console.WriteLine();
+                                    Console.WriteLine($"Resultado da Subtração: {PrimeiroNum} - {SegundoNum} = {Valor}");
+                                    Console.WriteLine();
+                                    Console.WriteLine("Para finalizar digite #SAIR ");
+                                
+                                break;
+
+                                case "/":
+                                    Valor = PrimeiroNum / SegundoNum;
+                                    Console.WriteLine();
+                                    Console.WriteLine($"Resultado da Subtração: {PrimeiroNum} / {SegundoNum} = {Valor}");
+                                    Console.WriteLine();
+                                    Console.WriteLine("Para finalizar digite #SAIR ou para continuar clique no ENTER");
+                                break;
+
+                                default:
+                                    throw new ArgumentOutOfRangeException();
+
+                        }
+                    Final = Console.ReadLine()!;
+                }
+            
+        }
+    }
+}
+           
